18367b72b527b168f1318364f206b486f7508199
Nguyen Ha Thai Bao <31185702+nguyenthaibao95@users.noreply.github.com>
2017-09-27 08:44:55 +0700

    Form Đăng nhập

--- QuanLyNhanSu/QuanLyNhanSu/Form1.cs
+++ QuanLyNhanSu/QuanLyNhanSu/Form1.cs
@@ -30,28 +30,83 @@ namespace QuanLyNhanSu
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            this.Hide();
+            KetNoi kn = new KetNoi();
+            kn.ShowDialog();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            Application.Exit();
         }
         private string layQHNhanVien(string tk, string mk)
         {
-            
+            string qh = null;
+            dr = cl.LayQuyenHanNV(tk, mk);
+            while (dr.Read())
+            {
+                qh = dr.GetString(0);
+            }
+            return qh;
         }
         private string layTenNhanVien(string manv)
         {
-           
+            string ten = null;
+            dr = cl.LayTenNhanVien(manv);
+            while (dr.Read())
+            {
+                ten = dr.GetString(0);
+            }
+            return ten;
         }
         private string layMaNhanVien(string tk)
         {
-            
+            string manv = null;
+            dr = cl.LayMaNhanVien(tk);
+            while (dr.Read())
+            {
+                manv = dr.GetString(0);
+            }
+            return manv;
         }
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
-            
+            if (!string.IsNullOrEmpty(txtTaiKhoan.Text))
+            {
+                if (!string.IsNullOrEmpty(txtMatKhau.Text))
+                {
+                    dt.Clear();
+                    dt = cl.dangnhap(txtTaiKhoan.Text, txtMatKhau.Text);
+                    if (dt.Rows[0]["err"].ToString() == "0")
+                    {
+                        QuanLy ql = new QuanLy();
+                        CT.DoiThongTin.ma = layMaNhanVien(txtTaiKhoan.Text);
+                        CT.DoiMatKhau.manv = layMaNhanVien(txtTaiKhoan.Text);
+                        CT.DoiMatKhau.taikhoan = txtTaiKhoan.Text;
+                        QuanLy.ten = layTenNhanVien(layMaNhanVien(txtTaiKhoan.Text));
+                        QuanLy.quyenhan = layQHNhanVien(txtTaiKhoan.Text, txtMatKhau.Text);
+                        this.Hide();
+                        ql.Show();
+                    }
+                    else
+                    {
+                        label2.Text = " Lỗi!! Sai tài khoản hoặc mật khẩu";
+                        txtTaiKhoan.Clear();
+                        txtMatKhau.Clear();
+                        txtTaiKhoan.Focus();
+                    }
+                }
+                else
+                {
+                    label2.Text = " Lỗi!! nhập mật khẩu";
+                    txtMatKhau.Focus();
+                }
+            }
+            else
+            {
+                label2.Text = " Lỗi!! Nhập tài khoản của bạn";
+                txtTaiKhoan.Focus();
+            }
         }
 
 
