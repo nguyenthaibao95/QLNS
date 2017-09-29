0f03e3da952ccccf060585037f55571aa9670cb0
nguyenthang92 <gadanhxe.vp@gmail.com>
2017-09-29 11:02:16 +0700

    Thống kê nhân viên có nghỉ phép

--- QuanLyNhanSu/QuanLyNhanSu/ThongKe/tkNVNghiCoPhep.cs
+++ QuanLyNhanSu/QuanLyNhanSu/ThongKe/tkNVNghiCoPhep.cs
@@ -24,25 +24,71 @@ namespace QuanLyNhanSu.ThongKe
         DateTime n;
         private void btXem_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                DateTime ngaydau = Convert.ToDateTime(Convert.ToInt32(cbThang.Text) + "/" + "01/" + Convert.ToInt32(cbNam.Text) + " ");
+                DateTime ngaycuoi = Convert.ToDateTime(Convert.ToInt32(cbThang.Text) + "/" + "29/" + Convert.ToInt32(cbNam.Text) + " ");
+                if (rdNTN.Checked == true)
+                {
+                    try
+                    {
+                        n = Convert.ToDateTime(Convert.ToInt32(cbThang.Text) + "/" + Convert.ToInt32(txtNgay.Text) + "/" + Convert.ToInt32(cbNam.Text));
+                        dt.Clear();
+                        dt = tkcl.tkNhanVienNghiCoPhep(n, n, 1);
+                        dtgv.DataSource = dt;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+
+                }
+                else
+                {
+                    txtNgay.Enabled = false;
+                    try
+                    {
+                        dt.Clear();
+                        dt = tkcl.tkNhanVienNghiCoPhep(ngaydau, ngaycuoi, 0);
+                        dtgv.DataSource = dt;
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nhập đầy đủ thông tin!!");
+            }
         }
         private void load()
         {
-            
+            txtNgay.Enabled = false;
+            cbThang.Enabled = false;
+            cbNam.Enabled = false;
+            btXem.Enabled = false;
         }
         private void tkNVNghiCoPhep_Load(object sender, EventArgs e)
         {
-            
+            load();
         }
 
         private void rdNTN_CheckedChanged(object sender, EventArgs e)
         {
-           
+            txtNgay.Enabled = true;
+            cbThang.Enabled = true;
+            cbNam.Enabled = true;
+            btXem.Enabled = true;
         }
 
         private void rdT_CheckedChanged(object sender, EventArgs e)
         {
-           
+            txtNgay.Enabled = false;
+            cbThang.Enabled = true;
+            cbNam.Enabled = true;
+            btXem.Enabled = true;
         }
     }
 }
