7ea6271426adb55505950f6a4ed126978d0c577e
HungduongMTA <31845257+HungduongMTA@users.noreply.github.com>
2017-10-01 08:05:43 +0700

    Form Lịch Sử Làm Việc

--- QuanLyNhanSu/QuanLyNhanSu/CT/tkLSLam.cs
+++ QuanLyNhanSu/QuanLyNhanSu/CT/tkLSLam.cs
@@ -18,10 +18,21 @@ namespace QuanLyNhanSu.CT
         {
             InitializeComponent();
         }
-        
+        CauLenh cl = new CauLenh();
+        DataTable dt = new DataTable();
+        string tennv = null, manv = null, tencv = null;
+        SqlDataReader dr;
         private void load()
         {
-            
+            dt.Clear();
+            dt = cl.LayctChucVu("0");
+            dataGridView1.DataSource = dt;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                string manhanvien = dataGridView1.Rows[i].Cells["MaNV"].Value.ToString();
+                dataGridView1.Rows[i].Cells["TenNV"].Value = laytennvtumanv(manhanvien);
+                dataGridView1.Rows[i].Cells["TenCV"].Value = laytencvtumanv(manhanvien);
+            }
 
         }
         private void tkLSLam_Load(object sender, EventArgs e)
@@ -30,20 +41,46 @@ namespace QuanLyNhanSu.CT
         }
         private string laytennvtumanv(string ma)
         {
-            
+            dr = cl.LayTenNhanVien(ma);
+            while (dr.Read())
+                tennv = dr.GetString(0);
+            return tennv;
         }
         private string laytencvtumanv(string ma)
         {
-            
+            dr = cl.LayChucVuTuMaNV(ma);
+            while (dr.Read())
+                tencv = dr.GetString(0);
+            return tencv;
         }
         private string laymanvtutennv(string ma)
         {
-            
+            dr = cl.layMaNVTuTenNV(txtTen.Text);
+            while (dr.Read())
+                manv = dr.GetString(0);
+            return manv;
         }
         private void btXem_Click(object sender, EventArgs e)
         {
-            
-            
+            try
+            {
+                manv = laymanvtutennv(txtTen.Text);
+                dt.Clear();
+                dt = cl.LayctChucVu(manv);
+                dataGridView1.DataSource = dt;
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    string manhanvien = dataGridView1.Rows[i].Cells["MaNV"].Value.ToString();
+                    dataGridView1.Rows[i].Cells["TenNV"].Value = laytennvtumanv(manhanvien);
+                    dataGridView1.Rows[i].Cells["TenCV"].Value = laytencvtumanv(manhanvien);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi hoặc không tồn tại tên nhân viên vừa nhập");
+
+            }
+
         }
     }
 }
