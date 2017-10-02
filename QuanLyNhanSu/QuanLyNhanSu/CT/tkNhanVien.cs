c46fd326e1cd4a7a5f00298741a626fe01c32440
HungCong <31845368+HungCong@users.noreply.github.com>
2017-10-02 15:26:23 +0700

    Form Bảo hiểm

--- QuanLyNhanSu/QuanLyNhanSu/CT/tkNhanVien.cs
+++ QuanLyNhanSu/QuanLyNhanSu/CT/tkNhanVien.cs
@@ -23,25 +23,47 @@ namespace QuanLyNhanSu.CT
         DataTable dt = new DataTable();
         private void lbH_Click(object sender, EventArgs e)
         {
-            
+            txtH.Text = "VD: Tên nhân viên, quê quán, chức vụ, phòng ban....";
         }
 
         private void txtH_Click(object sender, EventArgs e)
         {
             txtH.Text = "";
         }
+
         private void load()
         {
-            
+            dt.Clear();
+            dt = cl.LayThongTinNV1("NV");
+            dataGridView1.DataSource = dt;
         }
         private void tkNhanVien_Load(object sender, EventArgs e)
         {
-            
+            load();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            if (!string.IsNullOrEmpty(txtH.Text))
+            {
+                if (txtH.Text.ToLower() == "all")
+                    load();
+                else
+                {
+                    int gt = 0;
+                    if (radioButton1.Checked == true)
+                        gt = 0;
+                    if (radioButton2.Checked == true)
+                        gt = 1;
+                    if (radioButton3.Checked == true)
+                        gt = 2;
+                    dt.Clear();
+                    dt = tkcl.tkNhanVien(txtH.Text, gt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            else
+                MessageBox.Show("Nhập nội dung cần tìm trước");
         }
 
     }
